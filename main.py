fd22b6687246a82aac18d79aa27a684b991af597
Raphael Berly <raphael.berly@blablacar.com>
2020-07-10 00:04:24 +0200

    Add basic logging to main.py file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import logging
+
 import yaml
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 from lib.mailer import Mailer
 from lib.scraper import Scraper
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
 
 credentials = yaml.safe_load(open('conf/credentials.yaml'))
 conf = yaml.safe_load(open('conf/conf.yaml'))
@@ -21,13 +26,17 @@ def format_results(shows):
     return days
 
 
+LOGGER.info('Scraping movie schedule...')
 results = Scraper.run()
 
+LOGGER.info('Generating HTML content...')
 environment = Environment(loader=FileSystemLoader('templates'), undefined=StrictUndefined)
 html = environment.get_template('email.html').render(results=format_results(results))
-
 payload = yaml.safe_load(open('conf/mailer.yaml'))['payload']
 payload['Messages'][0]['HTMLPart'] = html
 
+LOGGER.info('Sending to mailing list...')
 mailer = Mailer(**credentials)
-print(mailer.send(payload))
+mailer.send(payload)
+
+LOGGER.info('Done')
